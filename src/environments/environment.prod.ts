ed34dea9052a3c3d54280692b4e48a80bf6b1cdd
Sergey <s_kulaev@bk.ru>
2022-05-13 01:33:34 +0700

    added prod command for test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   serverAddres: 'http://10.3.3.20:1337',
+  //serverAddres: 'http://10.3.3.20:1337',
+  //labServerAddres: 'http://10.3.200.20:3000',
+  intermediaryServerAddres: 'http://10.3.3.20:3001',
   production: true,
 };
