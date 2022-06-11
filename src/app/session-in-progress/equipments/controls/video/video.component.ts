fd014e8666fddb572ee113604fd108fa8ccbf54c
Sergey <s_kulaev@bk.ru>
2022-06-12 06:38:13 +0700

    fixed playback video

--- src/app/session-in-progress/equipments/controls/video/video.component.ts
+++ src/app/session-in-progress/equipments/controls/video/video.component.ts
@@ -1,16 +1,33 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.scss']
 })
-export class VideoComponent implements OnInit {
+export class VideoComponent implements OnInit, OnDestroy {
 
   @Input()
   stream_url?: string;
   constructor() { }
 
+  private onDestroy$ = new Subject<boolean>();
+
+  ngOnDestroy() {
+    this.onDestroy$.next(true);
+    this.onDestroy$.complete();
+  }
+
   ngOnInit(): void {
+    fromEvent(window, 'focus').pipe(takeUntil(this.onDestroy$)).subscribe(this.reset.bind(this))
+  }
+
+  visible = true;
+
+  reset(){
+    this.visible = false;
+    setTimeout(()=> this.visible = true);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,21 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// export const environment = {
+//   serverAddres: 'http://localhost:1337',
+//   //serverAddres: 'http://10.3.3.20:1337',
+//   //labServerAddres: 'http://10.3.200.20:3000',
+//   intermediaryServerAddres: 'http://localhost:3001',
+//   production: false,
+// };
+
 export const environment = {
-  serverAddres: 'http://localhost:1337',
+  serverAddres: 'http://10.3.3.20:1337',
   //serverAddres: 'http://10.3.3.20:1337',
   //labServerAddres: 'http://10.3.200.20:3000',
-  intermediaryServerAddres: 'http://localhost:3001',
-  production: false,
+  intermediaryServerAddres: 'http://10.3.3.20:3001',
+  production: true,
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
